c0e6e7885c38e8d803b9e4fdb3cd9803004896cd
imalovee <godsloveenyie@gmail.com>
2025-01-23 16:29:44 +0100

    completed the register and logn UI

--- lib/features/authentication/views/login_screen.dart
+++ lib/features/authentication/views/login_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sca_dchat_app/shared/colors.dart';
+import 'package:sca_dchat_app/shared/constants.dart';
+import 'package:sca_dchat_app/shared/notification/app_route.dart';
+
+import '../../../shared/notification/app_route_strings.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,8 +14,93 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+   TextEditingController emailController = TextEditingController();
+   TextEditingController passwordController = TextEditingController();
+    bool obscureText = false;
+ 
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+       centerTitle: true,
+        title: Text('Login', style: style.copyWith(
+          fontSize: 25,
+          fontWeight: FontWeight.w700
+        ),),
+        actions: [
+          TextButton
+          (onPressed: (){
+             AppRouter.pushReplace(AppRouteStrings.registerScreen);
+          }, 
+          child: Text('Signup', style: style.copyWith(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: AppColors.appColor
+          ),))
+        ],
+      ),
+      body: Padding(padding: EdgeInsets.all(16),
+      child: Form(
+        key: formKey,
+        child: Column(
+          children: [
+             AppTextInput(
+                label: 'Email Address',
+                  controller: emailController,
+                  validator: (a) {
+                    if(!emailRegex.hasMatch(a ?? "")){
+                      return 'Invalid Email';
+                    }
+                  },
+              ),
+              SizedBox(height: 12,),
+              AppTextInput(
+                label: 'Password',
+                  controller: passwordController,
+                  inputFormatter: [
+                            FilteringTextInputFormatter.deny(RegExp(r' '))
+                          ],
+                  validator: (a) {
+                    (a ?? '').length > 6 ? null : "Invald Password";
+                  },
+                  obscure: obscureText,
+                  suffixIcon: TextButton(onPressed: (){
+                      setState(() {
+                        obscureText = !obscureText;
+                      });
+                  }, 
+                  child: Text('show', style: style.copyWith(
+                    fontSize: 15,
+                fontWeight: FontWeight.w300,
+                color: AppColors.appColor
+                  ),)),
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              AppButton(
+            text: 'Login',
+            action: (){
+              if (formKey.currentState?.validate() ?? false) {
+                AppRouter.pushReplace(AppRouteStrings.homeScreen);
+              
+              }
+            },
+            ),
+             SizedBox(height: 16,),
+            Text('Forgot your password?',
+            style: style.copyWith(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: AppColors.appColor
+            ),
+            textAlign: TextAlign.center,
+            )
+          ],
+        ),
+      ),
+      ),
+    );
   }
 }
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -9,18 +9,25 @@ const style = TextStyle(
   fontWeight: FontWeight.w400,
 );
 
+final emailRegex = RegExp(
+    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
 class AppTextInput extends StatelessWidget {
   const AppTextInput(
       {super.key,
       this.validator,
       this.controller,
       this.label,
-      this.inputFormatter});
+      this.inputFormatter, 
+      this.suffixIcon,  
+      this.obscure = false});
 
   final TextEditingController? controller;
   final String? Function(String?)? validator;
   final String? label;
   final List<TextInputFormatter>? inputFormatter;
+  final Widget? suffixIcon; 
+  final bool obscure;
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +35,17 @@ class AppTextInput extends StatelessWidget {
       validator: validator,
       controller: controller,
       inputFormatters: inputFormatter,
+      obscureText: obscure,
       decoration: InputDecoration(
+        suffixIcon: suffixIcon,
         filled: true,
-        fillColor: AppColors.grey,
+        fillColor: Colors.grey.shade300,
         labelText: label,
+        labelStyle: style.copyWith(
+          color: Colors.grey.shade500,
+          fontSize: 16,
+          fontWeight: FontWeight.w400
+        ),
         border: OutlineInputBorder(),
         enabledBorder: OutlineInputBorder(
           borderSide: BorderSide(
